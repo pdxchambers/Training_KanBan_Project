8c42a97c5aed0f9dbd546036332c52c14f4afa8d
Julien Chambers <pdxchambers@gmail.com>
2019-12-26 15:53:58 -0800

    first pass commit

--- Pluralsight Projects/JS-KanbanBoard/js/kanban.js
+++ Pluralsight Projects/JS-KanbanBoard/js/kanban.js
@@ -16,7 +16,45 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  let item = document.createElement('div');
+  item.classList.add('item');
+  item.setAttribute('id', 'item-' + order);
+  item.setAttribute('draggable', 'true');
 };
 
+const dragItem =  document.querySelector('.item[draggable]')
+dragItem.addEventListenter('dragstart', (event) => {
+  event.DataTransfer.setData('text', event.target.id);
+});
+dragItem.addEventListenter('dragend', (event) => {
+  event.DataTransfer.clearData();
+});
+
+let input = document.createElement('input');
+item.apppendChild(input);
+
+const save_btn = document.createElement('button'); 
+save_btn.innerHTML('Save');
+save_btn.addEventListener('click', () => {
+  error = '';
+  if(input.value != ''){
+    order++;
+    item.innerHTML(input.value);
+    adding = false;
+  } else {
+    error = message;
+  }
+  item.appendChild(save_btn);
+  return item;
+})
+
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    event.darget.appendChild(document.getElementById(id));
+  })
+  element.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  })
 });
