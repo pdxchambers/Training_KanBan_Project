ab3ad8a13aa8067b4ec2655b64695cd512993635
Julien Chambers <pdxchambers@gmail.com>
2019-12-26 16:25:10 -0800

    Commit final copy

--- Pluralsight Projects/JS-KanbanBoard/js/kanban.js
+++ Pluralsight Projects/JS-KanbanBoard/js/kanban.js
@@ -18,43 +18,41 @@ add_btn.addEventListener('click', () => {
 const create_item = () => {
   let item = document.createElement('div');
   item.classList.add('item');
-  item.setAttribute('id', 'item-' + order);
-  item.setAttribute('draggable', 'true');
-};
+  item.id = 'item-' + order;
+  item.draggable = true;
 
-const dragItem =  document.querySelector('.item[draggable]')
-dragItem.addEventListenter('dragstart', (event) => {
-  event.DataTransfer.setData('text', event.target.id);
-});
-dragItem.addEventListenter('dragend', (event) => {
-  event.DataTransfer.clearData();
-});
+  item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
+  
+  item.addEventListener('dragend', event => event.dataTransfer.clearData() );
 
-let input = document.createElement('input');
-item.apppendChild(input);
+  let input = document.createElement('input');
+  item.appendChild(input);
 
-const save_btn = document.createElement('button'); 
-save_btn.innerHTML('Save');
-save_btn.addEventListener('click', () => {
-  error = '';
-  if(input.value != ''){
-    order++;
-    item.innerHTML(input.value);
-    adding = false;
-  } else {
-    error = message;
-  }
+  let save_btn = document.createElement('button'); 
+  save_btn.innerHTML = 'Save';
+  save_btn.addEventListener('click', () => {
+    error.innerHTML =  '';
+    if(input.value != ''){
+      order += 1;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = message;
+    }
+  });
   item.appendChild(save_btn);
   return item;
-})
+};
+
+
 
 document.querySelectorAll('.drop').forEach(element => {
-  element.addEventListener('drop', (event) => {
+  element.addEventListener('drop', event => {
     event.preventDefault();
     const id = event.dataTransfer.getData('text');
-    event.darget.appendChild(document.getElementById(id));
+    event.target.appendChild(document.getElementById(id));
   })
-  element.addEventListener('dragover', (event) => {
+  element.addEventListener('dragover', event => {
     event.preventDefault();
   })
 });
